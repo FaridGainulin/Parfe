0e01ead599b3febf6bc3075acf80aefe1ab822f3
unknown <farid.gai0in@gmail.com>
2024-07-04 04:35:25 +0600

    finish

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -118,8 +118,78 @@ function initSwiperBig() {
   })
 }
 
+function initSwiperDesktop() {
+  const swiper = new Swiper('.swiper-desktop', {
+    speed: 450,
+    effect: 'coverflow',
+    rewind: true,
+    pagination: {
+      el: '.desktop-pagination',
+      type: 'progressbar',
+    },
+    updateOnWindowResize: true,
+    spaceBetween: 20,
+    breakpoints: {
+      200: {
+        slideToClickedSlide: true,
+        coverflowEffect: {
+          rotate: 60,
+          stretch: 14,
+          depth: 500,
+          scale: 0.7,
+          modifier: 1,
+          slideShadows: false,
+        },
+        allowTouchMove: true,
+      },
+      1024: {
+        effect: 'slide',
+        navigation: {
+          nextEl: '.button-next',
+          prevEl: '.button-prev',
+        },
+        slidesPerView: 2,
+        spaceBetween: 20,
+        loop: true,
+        slideToClickedSlide: true,
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 0,
+          scale: 0,
+          modifier: 0,
+          slideShadows: false,
+        },
+        allowTouchMove: true,
+      },
+      1780:{
+        effect: 'slide',
+        navigation: {
+          nextEl: '.button-next',
+          prevEl: '.button-prev',
+        },
+        slidesPerView: 3,
+        spaceBetween: 20,
+        loop: true,
+        slideToClickedSlide: true,
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 0,
+          scale: 0,
+          modifier: 0,
+          slideShadows: false,
+        },
+        allowTouchMove: true,
+      }
+    },
+  })
+}
+
+
 $(document).ready(function () {
   initSwiperFinances()
   initSwiperAbout()
   initSwiperBig()
+  initSwiperDesktop()
 })
